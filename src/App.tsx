51dee271922305ef9417028e04ae6cb690153e7a
Johan Duque <johan.duque15@gmail.com>
2026-04-17 19:56:13 +0200

    se agrega PublicLayout y la navegacion por rutas

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import ServicesPage from "./pages/ServicesPage"
 import BookingPage from "./pages/BookingPage"
 import LoginPage from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
+import PublicLayout from "./layouts/PublicLayout/PublicLayout"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}/>
-      <Route path="/servicios" element={<ServicesPage />} />
-      <Route path="/reservar" element={<BookingPage />}/> 
-      <Route path="/login" element={<LoginPage />}/>
-      <Route path="registro" element={<RegisterPage />}/>
+      <Route element={<PublicLayout />} >
+        <Route path="/" element={<HomePage />} />
+        <Route path="/servicios" element={<ServicesPage />} />
+        <Route path="/reservar" element={<BookingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="registro" element={<RegisterPage />} />
+      </Route>
     </Routes>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,17 @@
-import Navbar from "../components/common/Navbar/Navbar";
 import Hero from "../components/common/Hero/Hero";
 import Services from "../components/common/Services/Services";
 import Barbers from "../components/common/Barbers/Barbers";
 import ReservationCTA from "../components/common/ReservationCTA/ReservationCTA";
-import Footer from "../components/common/Footer/Footer";
+
 
 export default function HomePage() {
     return(
-        <div className="min-h-screen bg-barber-black text-barber-gold">
-            <Navbar />
-            <Hero />
-            <Services />
-            <Barbers />
-            <ReservationCTA />
-            <Footer />
-        </div>
+        <>
+        <Hero />
+        <Services />
+        <Barbers />
+        <ReservationCTA />        
+        </>
+
     )
 }
