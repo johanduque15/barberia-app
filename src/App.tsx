70fb4ec8814ac21f160f7382398d9dc97c284029
Johan Duque <johan.duque15@gmail.com>
2026-05-13 08:39:40 +0200

    conexion panel admin con el back

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         <Route
           path="/admin"
           element={
-            <ProtectedRoute>
+            <ProtectedRoute requiredRole="admin">
               <AdminPage />
             </ProtectedRoute>
           }
